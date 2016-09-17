6a9fd44b75bd7478e5e810f5bef8202d60b4bdaf
Jeremy Price <donomii@gmail.com>
2016-09-17 16:30:44 +0200

    Add command line option to tpd-recents

--- tpb-recents.fish
+++ tpb-recents.fish
@@ -1,3 +1,6 @@
-for x in ( wget -O- https://thepiratebay.org/recent | perl -ne'while(/(magnet.+?)"/sg){print $1."\n";}')
-    go run main.go --dir='./' --pick=asdf $x &
+#!/usr/local/bin/fish
+set -x https_proxy
+set -x http_proxy
+for x in ( wget -O- $argv[1] | perl -ne'while(/(magnet:.+?)"/sg){print $1."\n";}')
+    ./main --dir='./subs/' --pick='srt$|sbv$|sub$|mpsub$|lrc$|cap$|smi$|sami$|rt$|vtt$|ttml$|dfxp$|scc$|stl$|cin$|asc$|txt$|nfo$' --timeout=120 $x
 end ;
