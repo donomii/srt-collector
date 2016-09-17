3ed5e6a56bbdd6b2c32ed67fa50db4a1e881b28d
Jeremy Price <donomii@gmail.com>
2016-09-17 20:10:16 +0200

    Fix example

--- tpb-recents.fish
+++ tpb-recents.fish
@@ -3,4 +3,4 @@ set -x https_proxy
 set -x http_proxy
 for x in ( wget -O- $argv[1] | perl -ne'while(/(magnet:.+?)"/sg){print $1."\n";}')
     ./main --dir='./subs/' --pick='srt$|sbv$|sub$|mpsub$|lrc$|cap$|smi$|sami$|rt$|vtt$|ttml$|dfxp$|scc$|stl$|cin$|asc$|txt$|nfo$' --timeout=120 $x
-end ;
+end
