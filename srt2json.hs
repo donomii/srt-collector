3058d9cc53e61da159e26218543007f84912bdd8
Donomii <donomii@gmail.com>
2016-09-26 22:02:53 +0000

    Working with utf8

--- srt2json.hs
+++ srt2json.hs
@@ -1,13 +1,16 @@
 import System.Environment
 import Text.Parsec
-import Text.Parsec.ByteString
+import Text.Parsec.ByteString.Lazy
 import Control.Monad
 import qualified Data.ByteString as B
+import qualified Data.ByteString.Lazy as L
 import Data.Text.Encoding
 import qualified Data.Text as T
 import Data.Text.Internal
 import Text.JSON
 import Data.List
+import Data.ByteString.UTF8
+import qualified Data.ByteString.Char8 as C
 
 -- type UString = Data.Text.Internal.Text
 type UString = String
@@ -19,20 +22,24 @@ main :: IO ()
       --[str] -> parseFromFile myParser str >>= either print print
       --_ -> error "please pass one argument with the file containing the text to parse"
 
-main = do
-    a <- getArgs
-    case a of
-      [str] -> do
-            result <- parseFromFile myParser str
-            putStr (case result of
-                    Left val -> show val
-                    Right val  ->  (concat(map (\x ->(encode x) ++ "\n") val)))
-      _ -> error "please pass one argument with the file containing the text to parse"
+main = 
+	do 
+	    c <- L.getContents
+	    result <- parse myParser "(stdin)" c
+	    putStr (case result of
+		    Left val -> show val
+		    Right val  ->  (concat(map (\x ->(encodeStrict x) ++ "\n") val)))
 
 --myParser :: Parser [(UString, (([UString], UString), ([UString], UString)), [UString])]
 myParser = do
+	     optional bom
              b <- blocks
              return (b)
+
+bom = do
+	oneOf [ '\xff', '\254' ]
+	oneOf [ '\xff', '\254' ]
+	return ()
 
 blocks = do
             bs <- manyTill block eof
@@ -43,7 +50,7 @@ block = do
             void $ myCrlf
             time <- timeLine
             t <- myText
-            void $ myCrlf
+            void $ optional myCrlf
             return (id,time,t)
 
 
@@ -65,6 +72,7 @@ timeLine = do
             void $ string " --> "
             finish <- time
             void $ myCrlf
+	    void $ optional myCrlf
             return (start,finish)
 myText = do
             t <- many1 textLine
@@ -72,8 +80,8 @@ myText = do
 
 textLine = do
             text <- many1 (noneOf "\r\n")
-            void $ myCrlf
-            return (text)
+            void $ optional myCrlf
+            return (toString (C.pack text))
 
 myCrlf = do
         optional cr
