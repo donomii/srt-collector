875afd12caeab6873910b1be06a7e403976299da
Jeremy Price <donomii@gmail.com>
2016-09-28 13:11:49 +0200

    Cleanup redundant and unnecessary syntax

--- srt2json.hs
+++ srt2json.hs
@@ -3,7 +3,6 @@ import Text.Parsec
 import Text.Parsec.ByteString
 import Control.Monad
 import qualified Data.ByteString as B
--- import qualified Data.ByteString.Lazy as L
 import Data.Text.Encoding
 import qualified Data.Text as T
 import Data.Text.Internal
@@ -16,27 +15,20 @@ import qualified Data.ByteString.Char8 as C
 type UString = String
 
 main :: IO ()
---main = do
-    --a <- getArgs
-    --case a of
-      --[str] -> parseFromFile myParser str >>= either print print
-      --_ -> error "please pass one argument with the file containing the text to parse"
-
 main =
         do
             c <- B.getContents
-        --result <- parse myParser "(stdin)" c
-            putStr  (deMonadify (parse myParser "(stdin)" c))
+            putStr  (formatOutput (parse myParser "(stdin)" c))
 
-deMonadify :: Either ParseError [(UString, (([UString], UString), ([UString], UString)), [UString])] -> String
-deMonadify result = case result of
+formatOutput :: Either ParseError [(UString, (([UString], UString), ([UString], UString)), [UString])] -> UString
+formatOutput result = case result of
                     Left val -> show val
                     Right val  ->  concat (map (\x ->(encodeStrict x) ++ "\n") val)
+
 myParser :: Parser [(UString, (([UString], UString), ([UString], UString)), [UString])]
 myParser = do
              optional bom
              blocks
-
 
 bom = do
         oneOf [ '\xff', '\254', '\239' ]
@@ -46,42 +38,37 @@ bom = do
 
 blocks = manyTill block eof
 
-
 block = do
             id <- num
-            void $ myCrlf
+            myCrlf
             time <- timeLine
             t <- myText
-            void $ optional myCrlf
+            optional myCrlf
             return (id,time,t)
 
-
--- num :: Parser UString
 num = many1 digit
-
 
 hms = sepBy num (char ':')
 
 time = do
         startHMS <- hms
-        void $ char ','
+        char ','
         startMilli <- many1 digit
         return (startHMS, startMilli)
 
 timeLine = do
             start <- time
-            void $ string " --> "
+            string " --> "
             finish <- time
-            void $ myCrlf
-            void $ optional myCrlf
+            myCrlf
+            optional myCrlf
             return (start,finish)
-myText = do
-            t <- many1 textLine
-            return (t)
+
+myText = many1 textLine
 
 textLine = do
             text <- many1 (noneOf "\r\n")
-            void $ optional myCrlf
+            optional myCrlf
             return (toString (C.pack text))
 
 myCrlf = do
