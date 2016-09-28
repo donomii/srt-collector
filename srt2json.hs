bd0e2576e1ce5b9671e8cfbc0300a991cc6384cc
Jeremy Price <donomii@gmail.com>
2016-09-28 13:04:44 +0200

    Working for utf8

--- srt2json.hs
+++ srt2json.hs
@@ -1,9 +1,9 @@
 import System.Environment
 import Text.Parsec
-import Text.Parsec.ByteString.Lazy
+import Text.Parsec.ByteString
 import Control.Monad
 import qualified Data.ByteString as B
-import qualified Data.ByteString.Lazy as L
+-- import qualified Data.ByteString.Lazy as L
 import Data.Text.Encoding
 import qualified Data.Text as T
 import Data.Text.Internal
@@ -22,28 +22,30 @@ main :: IO ()
       --[str] -> parseFromFile myParser str >>= either print print
       --_ -> error "please pass one argument with the file containing the text to parse"
 
-main = 
-	do 
-	    c <- L.getContents
-	    result <- parse myParser "(stdin)" c
-	    putStr (case result of
-		    Left val -> show val
-		    Right val  ->  (concat(map (\x ->(encodeStrict x) ++ "\n") val)))
+main =
+        do
+            c <- B.getContents
+        --result <- parse myParser "(stdin)" c
+            putStr  (deMonadify (parse myParser "(stdin)" c))
 
---myParser :: Parser [(UString, (([UString], UString), ([UString], UString)), [UString])]
+deMonadify :: Either ParseError [(UString, (([UString], UString), ([UString], UString)), [UString])] -> String
+deMonadify result = case result of
+                    Left val -> show val
+                    Right val  ->  concat (map (\x ->(encodeStrict x) ++ "\n") val)
+myParser :: Parser [(UString, (([UString], UString), ([UString], UString)), [UString])]
 myParser = do
-	     optional bom
-             b <- blocks
-             return (b)
+             optional bom
+             blocks
+
 
 bom = do
-	oneOf [ '\xff', '\254' ]
-	oneOf [ '\xff', '\254' ]
-	return ()
+        oneOf [ '\xff', '\254', '\239' ]
+        oneOf [ '\xff', '\254', '\187' ]
+        oneOf [ '\xff', '\254', '\187', '\191' ]
+        return ()
 
-blocks = do
-            bs <- manyTill block eof
-            return bs
+blocks = manyTill block eof
+
 
 block = do
             id <- num
@@ -54,10 +56,9 @@ block = do
             return (id,time,t)
 
 
-num :: Parser UString
-num = do
-    n <- many1 digit
-    return (n)
+-- num :: Parser UString
+num = many1 digit
+
 
 hms = sepBy num (char ':')
 
@@ -66,13 +67,13 @@ time = do
         void $ char ','
         startMilli <- many1 digit
         return (startHMS, startMilli)
-        
+
 timeLine = do
             start <- time
             void $ string " --> "
             finish <- time
             void $ myCrlf
-	    void $ optional myCrlf
+            void $ optional myCrlf
             return (start,finish)
 myText = do
             t <- many1 textLine
@@ -91,4 +92,4 @@ cr :: Parser ()
 cr = do
         (char '\r')
         return ()
-            
+
